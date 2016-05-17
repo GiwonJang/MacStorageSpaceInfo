c6b9e0ba02a96848d5eb9b5e20ea125017938d4d
Giwon Jang <spike.itsme@gmail.com>
2016-05-17 13:03:25 +0900

    Initial Commit

---
+++ MacStorageSpaceInfo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MacStorageSpaceInfo
+//
+//  Created by JANGGIWON on May 17, 2016.
+//  Copyright © 2016 Handicraft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
